9f0d085e1e79d70370798db06130c98d17a5119e
lizard219 <Moschkau1987@gmxde>
2016-09-25 17:30:42 +0200

    reworked collision detection

--- Game/GameManager.py
+++ Game/GameManager.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.locals import *
 from Objects.Ball import Ball
+import math, sys
 
 class GameManager:
     def __init__(self):
-        self.ball_number = 2
+        self.ball_number = 1
         self.balls       = []
-        self.gravity     = 0.5
+        self.gravity     = 1
         self.friction    = 0.00001
 
         for x in range(0, self.ball_number):
@@ -23,13 +24,14 @@ class GameManager:
 
 
     def show_debug(self, app, surface):
-        counter = 1
+        counter = 0
         for ball in self.balls:
-            y_pos = counter * 70
+            y_pos = counter * 80 + 10
             surface.blit(app.font.Font(None, 20).render("Ball %d" % counter, 1, (250, 240, 230)), (20, y_pos + 0))
             surface.blit(app.font.Font(None, 20).render("Maximale Höhe %.2f" % ball.max_height, 1, (250, 240, 230)), (20, y_pos + 15))
             surface.blit(app.font.Font(None, 20).render("aktuelle Höhe %.2f" % ball.y, 1, (250, 240, 230)), (20, y_pos + 30))
             surface.blit(app.font.Font(None, 20).render("Start-Höhe %d" % ball.init_y, 1, (250, 240, 230)), (20, y_pos + 45))
+            surface.blit(app.font.Font(None, 20).render("letzte Höhe %.2f" % ball.last_height, 1, (250, 240, 230)), (20, y_pos + 60))
 
             counter += 1
 
@@ -47,31 +49,43 @@ class GameManager:
         balls = self.balls
 
         for ball in balls:
-            ball.move()
+            ball.move(self.gravity)
 
     def detect_collisions(self):
         self.detect_wall_collisions()
 
+    def handle_wall_collision(self, ball, wall_position):
+        gravity = self.gravity
+
+        original_speed = ball.speed_y - gravity
+        orig_pos_y     = ball.y - ( original_speed + gravity / 2 )
+
+        time_to_wall   = (math.sqrt(2 * gravity * wall_position - 2 * gravity * orig_pos_y + original_speed**2) - original_speed ) / gravity
+        speed_at_wall  = gravity * time_to_wall + original_speed
+
+        remaining_time = 1 - time_to_wall
+
+        bounce_distance = -gravity / 2 * remaining_time**2 + speed_at_wall * remaining_time
+
+        final_speed  = -gravity * remaining_time + speed_at_wall
+
+        ball.y       = wall_position - bounce_distance
+        ball.speed_y = -final_speed
+
     def detect_wall_collisions(self):
-        balls = self.balls
+        balls   = self.balls
 
         for ball in balls:
             if ( ( ball.x + ball.radius >= 800 ) or ( ball.x - ball.radius <= 0 ) ):
                 ball.speed_x *= -1
-            else:
-                ball.speed_x *= ( 1 - self.friction )
 
             if ( ( ball.y + ball.radius >= 600 ) ):
-                trespass_distance = ball.y + ball.radius - 600
-                percentage        = trespass_distance / ball.speed_y
-                over_gravity      = self.gravity * percentage
+                wall = 600 - ball.radius
 
-                ball.speed_y -= over_gravity
-                ball.speed_y *= -1
-                ball.speed_y += over_gravity
+                self.handle_wall_collision(ball, wall)
 
-                ball.y = 600 - trespass_distance - ball.radius
-            else:
-                ball.speed_y += self.gravity
-                ball.speed_y *= ( 1 - self.friction )
+            if ( ( ball.y - ball.radius <= 0 ) ):
+                wall = 0 + ball.radius
+
+                self.handle_wall_collision(ball, wall)
 
--- Objects/Ball.py
+++ Objects/Ball.py
@@ -6,21 +6,31 @@ class Ball:
     def __init__(self):
         self.radius     = 25
         self.x          = random.randint(self.radius, 800-self.radius)
-        self.y          = random.randint(0, 300)
-        self.speed_x    = 0
+        self.y          = 100 #random.randint(self.radius, 600-self.radius)
+        self.speed_x    = random.randint(0, 20) - 10
         self.speed_y    = 0
         self.color      = (150,0,0)
-        self.max_height = 600
+        self.max_height  = self.y
+        self.last_height = self.y
 
+        self.project_y  = 0
         self.init_y     = self.y
 
     def draw(self, app, surface):
         app.draw.circle(surface, self.color,(int(self.x), int(self.y)),self.radius)
 
-    def move(self):
-        self.x += self.speed_x
-        self.y += self.speed_y
+    def move (self, gravity):
+        projected_x_dist = self.speed_x
+        projected_y_dist = self.speed_y + gravity / 2
+
+        self.x           += projected_x_dist
+        self.y           += projected_y_dist
+
+        self.speed_y     += gravity
 
         if (self.y < self.max_height):
             self.max_height = self.y
+
+        if ((projected_y_dist < 0 and self.speed_y) > 0):
+            self.last_height = self.y
 
